9321b70904e6ef4fda23ddd46c199e48c56e01e2
unknown <lochaydeguerreiro@hotmail.com>
2025-02-05 16:59:50 -0300

    Atualização do Blog

--- js/script.js
+++ js/script.js
@@ -65,3 +65,64 @@ function includeHTML() {
               });
           });
       });
+
+
+      document.addEventListener("DOMContentLoaded", function() {
+        fetch("catalogo.json")
+        .then(response => response.json())
+        .then(products => {
+            const container = document.getElementById("product-container");
+            
+            products.forEach(product => {
+                const card = document.createElement("div");
+                card.className = "col";
+                card.innerHTML = `
+                    <div class="card h-100 border-0 rounded-pill text-center">
+                        <img src="${product.image}" class="card-img-top" alt="${product.name}">
+                        <div class="card-body">
+                            <div class="rating">
+                                <span class="stars" data-rating="${product.rating}"></span>
+                                <span class="rating-text">${product.rating}</span>
+                            </div>
+                            <a href="${product.url}" class="card-title" target="_blank">${product.name}</a>
+                            <p style="display: none!important;">${product.price}</p>
+                        </div>
+                    </div>
+                `;
+                container.appendChild(card);
+            });
+    
+            document.querySelectorAll(".stars").forEach(el => {
+                let rating = parseFloat(el.getAttribute("data-rating"));
+                let filledStars = Math.round(rating);
+                el.innerHTML = "★".repeat(filledStars) + "☆".repeat(5 - filledStars);
+            });
+        })
+        .catch(error => console.error("Erro ao carregar os produtos:", error));
+    });
+    
+
+    let products = [];
+
+    function addProduct() {
+        let name = document.getElementById("name").value;
+        let image = document.getElementById("image").value;
+        let rating = parseFloat(document.getElementById("rating").value);
+        let url = document.getElementById("url").value;
+
+        let newProduct = { name, image, rating, url };
+
+        products.push(newProduct);
+
+        document.getElementById("jsonOutput").value = JSON.stringify({ products }, null, 2);
+    }
+
+    function downloadJSON() {
+        let dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify({ products }, null, 2));
+        let downloadAnchor = document.createElement("a");
+        downloadAnchor.setAttribute("href", dataStr);
+        downloadAnchor.setAttribute("download", "catalogo.json");
+        document.body.appendChild(downloadAnchor);
+        downloadAnchor.click();
+        document.body.removeChild(downloadAnchor);
+    }
